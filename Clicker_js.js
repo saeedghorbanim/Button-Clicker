5310007ef57f8fa9ee4acc13e719b35027a9a854
saeedghorbanim <animation10733@yahoo.com>
2021-06-28 22:04:30 -0500

    Needs update

---
+++ Clicker_js.js
@@ -0,0 +1,15 @@
+function remove (element)
+{
+    element.remove();
+}
+
+function logout (element)
+{
+    element.innerHTML = "logout";
+
+}
+
+function like (element)
+{
+    element = alert("Ninja was liked");
+}
